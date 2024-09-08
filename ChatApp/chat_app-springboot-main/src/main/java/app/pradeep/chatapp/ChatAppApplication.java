149f07834f601a698c785ce66903e5df0ef00c60
Pradeep-Chandra-17 <94434721+Pradeep-Chandra-G@users.noreply.github.com>
2024-09-08 13:17:16 +0530

    Update ChatAppApplication.java

--- ChatApp/chat_app-springboot-main/src/main/java/app/pradeep/chatapp/ChatAppApplication.java
+++ ChatApp/chat_app-springboot-main/src/main/java/app/pradeep/chatapp/ChatAppApplication.java
@@ -2,13 +2,17 @@ package app.pradeep.chatapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * The main class for the Chat App application.
  */
 @SpringBootApplication
-public class ChatAppApplication {
+@RestController
+public class ChatAppApplication{
 
+	@RequestMapping("/")
 	public static void main(String[] args) {
 		SpringApplication.run(ChatAppApplication.class, args);
 	}
